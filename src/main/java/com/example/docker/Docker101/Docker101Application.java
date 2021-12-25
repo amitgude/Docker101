df340af73b38e9008e805c0d6cab8778e8d9d06e
Amit Gude <gudeamit148@gmail.com>
2021-12-26 00:50:09 +0530

    Initial commit

---
+++ src/main/java/com/example/docker/Docker101/Docker101Application.java
@@ -0,0 +1,13 @@
+package com.example.docker.Docker101;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Docker101Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Docker101Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/docker/Docker101/Docker101ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker.Docker101;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Docker101ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
